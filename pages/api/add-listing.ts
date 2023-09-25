172763233bd9e7a20c33c8dd2ac62986e2807841
theibrahim <iamibrahimmemon@gmail.com>
2023-09-25 09:58:05 +0500

    used supabase and ximilar here

--- pages/api/add-listing.ts
+++ pages/api/add-listing.ts
@@ -1,16 +1,19 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "@/prisma/client";
-import { supabase, supabaseUrl } from "@/supabase/client";
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../prisma/client';
+import { createClient } from '@supabase/supabase-js';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: { persistSession: false },
+});
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
     const listing = req.body.listing;
 
     if (!listing) {
-      return res.status(400).json({ message: "No listing provided!" });
+      return res.status(400).json({ message: 'No listing provided!' });
     }
 
     try {
@@ -18,42 +21,30 @@ export default async function handler(
       let brandImagePath: any = null;
 
       if (listing.mainImage) {
-        const uploadResponse = await supabase.storage
-          .from("listings")
-          .upload(
-            `mainImage-${Date.now()}.png`,
-            Buffer.from(listing.mainImage, "base64"),
-            {
-              contentType: "image/png",
-            }
-          );
+        const uploadResponse = await supabase.storage.from('listings').upload(`mainImage-${Date.now()}.png`, Buffer.from(listing.mainImage, 'base64'), {
+          contentType: 'image/png'
+        });
         if (uploadResponse.error) {
           throw new Error(uploadResponse.error.message);
         }
         const imagePath = uploadResponse.data.path;
 
         mainImagePath = {
-          url: `${supabaseUrl}/storage/v1/object/public/${"listings"}/${imagePath}`,
+          url: `${supabaseUrl}/storage/v1/object/public/${'listings'}/${imagePath}`
         };
       }
 
       if (listing.brandImage) {
-        const uploadResponse = await supabase.storage
-          .from("listings")
-          .upload(
-            `brandImage-${Date.now()}.png`,
-            Buffer.from(listing.brandImage, "base64"),
-            {
-              contentType: "image/png",
-            }
-          );
+        const uploadResponse = await supabase.storage.from('listings').upload(`brandImage-${Date.now()}.png`, Buffer.from(listing.brandImage, 'base64'), {
+          contentType: 'image/png'
+        });
         if (uploadResponse.error) {
           throw new Error(uploadResponse.error.message);
         }
         const imagePath = uploadResponse.data.path;
 
         brandImagePath = {
-          url: `${supabaseUrl}/storage/v1/object/public/${"listings"}/${imagePath}`,
+          url: `${supabaseUrl}/storage/v1/object/public/${'listings'}/${imagePath}`
         };
       }
 
@@ -63,18 +54,12 @@ export default async function handler(
         subCategoryTwo: listing.subCategoryTwo,
         mainImage: mainImagePath,
         brandImage: brandImagePath,
-        tags: {
-          create: listing.tags.map((x) => {
-            return {
-              name: x.name,
-              value: String(x.value),
-            };
-          }),
-        },
-      };
+        tags: listing.tags
+      }
+
 
       const createdListing = await prisma.listing.create({
-        data: payload,
+        data: payload
       });
 
       res.status(200).json(createdListing);
@@ -83,6 +68,6 @@ export default async function handler(
       res.status(500).json({ message: error.message });
     }
   } else {
-    res.status(405).json({ message: "Method not allowed." });
+    res.status(405).json({ message: 'Method not allowed.' });
   }
 }
