ea99fe0662d0e201a3d6d169031c705694626e6a
theibrahim <iamibrahimmemon@gmail.com>
2023-09-26 11:31:59 +0500

    improved google-vision api

--- pages/api/getTags.js
+++ pages/api/getTags.js
@@ -1,16 +1,19 @@
 import { ImageAnnotatorClient } from '@google-cloud/vision';
+import ntc from 'ntcjs';
 
 const credentials = JSON.parse(Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64, 'base64').toString());
-
 const client = new ImageAnnotatorClient({ credentials });
-
+const rgbToHex = (r, g, b) => {
+    return '#' +
+        r.toString(16).padStart(2, '0') +
+        g.toString(16).padStart(2, '0') +
+        b.toString(16).padStart(2, '0');
+};
 
 export default async (req, res) => {
     try {
         const base64Image = req.body.image;
         const imageType = req.body.type;
-
-
 
         if (!base64Image || !imageType) {
             return res.status(400).json({ error: "Missing required parameters." });
@@ -24,22 +27,39 @@ export default async (req, res) => {
             }
             bufferData = Buffer.from(imageData[1], 'base64');
         } else {
-            // If the base64 string doesn't have the prefix, assume it's raw base64 data.
             bufferData = Buffer.from(base64Image, 'base64');
         }
 
-        let tags = [];
-
+        let requestFeatures = [];
         if (imageType === "main") {
-            // Using Google Vision Label Detection
-            const [result] = await client.labelDetection(bufferData);
-            tags = result.labelAnnotations.map(label => label.description);
+            requestFeatures = [
+                { type: 'TEXT_DETECTION' },
+                { type: 'IMAGE_PROPERTIES' },
+                { type: 'LOGO_DETECTION' },
+                { type: 'LABEL_DETECTION' }
+            ];
         } else if (imageType === "brandTag") {
-            // Using Google Vision's Text Detection
-            const [result] = await client.textDetection(bufferData);
-            const detections = result.textAnnotations;
-            tags = detections.map(text => text.description);
+            requestFeatures = [
+                { type: 'TEXT_DETECTION' },
+                { type: 'LOGO_DETECTION' }
+            ];
         }
+
+        const [result] = await client.annotateImage({ image: { content: bufferData }, features: requestFeatures });
+
+        const labelTags = result.labelAnnotations?.map(label => label.description) || [];
+        const textTags = result.textAnnotations?.map(t => t.description) || [];
+
+        // Convert RGB values to human-readable color names
+        const colorTags = result.imagePropertiesAnnotation?.dominantColors.colors.map(color => {
+            const hexColor = rgbToHex(Math.round(color.color.red), Math.round(color.color.green), Math.round(color.color.blue));
+            const nameMatch = ntc.name(hexColor);
+            return nameMatch[1]; // returning the color name
+        }) || [];
+
+        const logoTags = result.logoAnnotations?.map(logo => logo.description) || [];
+
+        const tags = [...labelTags, ...textTags, ...colorTags, ...logoTags];
 
         res.status(200).json({ tags: tags });
 
