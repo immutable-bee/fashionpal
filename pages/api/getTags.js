c3e0553052ce0935eb0f09bafa836e34024ae9dc
theibrahim <iamibrahimmemon@gmail.com>
2023-09-27 14:10:18 +0500

    remove bg

--- pages/api/getTags.js
+++ pages/api/getTags.js
@@ -1,14 +1,34 @@
 import { ImageAnnotatorClient } from '@google-cloud/vision';
 import ntc from 'ntcjs';
+import axios from 'axios';
 
 const credentials = JSON.parse(Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64, 'base64').toString());
 const client = new ImageAnnotatorClient({ credentials });
-const rgbToHex = (r, g, b) => {
-    return '#' +
-        r.toString(16).padStart(2, '0') +
-        g.toString(16).padStart(2, '0') +
-        b.toString(16).padStart(2, '0');
+
+const REMOVE_BG_API_KEY = process.env.REMOVE_BG_API_KEY;
+const REMOVE_BG_URL = 'https://api.remove.bg/v1.0/removebg';
+
+const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => x.toString(16).padStart(2, '0')).join('');
+
+const removeBgApiCall = async (base64) => {
+    try {
+        const formData = new FormData();
+        formData.append('image_file_b64', base64);
+        formData.append('size', 'auto');
+
+        const response = await axios.post(REMOVE_BG_URL, formData, {
+            headers: {
+                'X-API-Key': REMOVE_BG_API_KEY
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error with remove.bg API:", error.response && error.response.data);
+        throw error;
+    }
 };
+
+
 
 export default async (req, res) => {
     try {
@@ -19,52 +39,34 @@ export default async (req, res) => {
             return res.status(400).json({ error: "Missing required parameters." });
         }
 
-        let bufferData;
-        if (base64Image.includes('base64,')) {
-            const imageData = base64Image.split('base64,');
-            if (imageData.length !== 2) {
-                return res.status(400).json({ error: "Invalid base64 format." });
-            }
-            bufferData = Buffer.from(imageData[1], 'base64');
-        } else {
-            bufferData = Buffer.from(base64Image, 'base64');
-        }
+        const bufferData = Buffer.from(base64Image.split('base64,')[1] || base64Image, 'base64');
+        const bgRemovedImage = await removeBgApiCall(bufferData.toString('base64'));
 
-        let requestFeatures = [];
-        if (imageType === "main") {
-            requestFeatures = [
-                { type: 'TEXT_DETECTION' },
-                { type: 'IMAGE_PROPERTIES' },
-                { type: 'LOGO_DETECTION' },
-                { type: 'LABEL_DETECTION' }
-            ];
-        } else if (imageType === "brandTag") {
-            requestFeatures = [
-                { type: 'TEXT_DETECTION' },
-                { type: 'LOGO_DETECTION' }
-            ];
-        }
+        const requestFeatures = imageType === "main" ?
+            [{ type: 'TEXT_DETECTION' }, { type: 'IMAGE_PROPERTIES' }, { type: 'LOGO_DETECTION' }, { type: 'LABEL_DETECTION' }, { type: 'WEB_DETECTION' }] :
+            [{ type: 'TEXT_DETECTION' }, { type: 'LOGO_DETECTION' }];
 
         const [result] = await client.annotateImage({ image: { content: bufferData }, features: requestFeatures });
 
-        const labelTags = result.labelAnnotations?.map(label => label.description) || [];
-        const textTags = result.textAnnotations?.map(t => t.description) || [];
+        const tags = [
+            ...result.labelAnnotations?.map(label => label.description) || [],
+            ...result.textAnnotations?.map(t => t.description) || [],
+            ...result.imagePropertiesAnnotation?.dominantColors.colors.map(color => {
+                const hexColor = rgbToHex(Math.round(color.color.red), Math.round(color.color.green), Math.round(color.color.blue));
+                return ntc.name(hexColor)[1];
+            }) || [],
+            ...result.logoAnnotations?.map(logo => logo.description) || []
+        ];
 
-        // Convert RGB values to human-readable color names
-        const colorTags = result.imagePropertiesAnnotation?.dominantColors.colors.map(color => {
-            const hexColor = rgbToHex(Math.round(color.color.red), Math.round(color.color.green), Math.round(color.color.blue));
-            const nameMatch = ntc.name(hexColor);
-            return nameMatch[1]; // returning the color name
-        }) || [];
-
-        const logoTags = result.logoAnnotations?.map(logo => logo.description) || [];
-
-        const tags = [...labelTags, ...textTags, ...colorTags, ...logoTags];
-
-        res.status(200).json({ tags: tags });
+        res.status(200).json({
+            tags: tags,
+            bgRemovedImage: bgRemovedImage.result,  // Base64 or a URL, depending on the API response
+            similarProducts: result.webDetection?.webEntities.map(entity => entity.description) || []
+        });
 
     } catch (error) {
         console.error("Error processing image:", error);
         res.status(500).json({ error: error.message });
     }
 };
+
