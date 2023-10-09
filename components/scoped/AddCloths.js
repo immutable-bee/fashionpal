18a0f0c816681cc2e36c2bf2612ade1bb64efee8
theibrahim <iamibrahimmemon@gmail.com>
2023-10-09 09:35:48 +0300

    padding

--- components/scoped/AddCloths.js
+++ components/scoped/AddCloths.js
@@ -586,7 +586,7 @@ function ImageUploader({ onBack, onFecth }) {
 
 
     return (
-        <div className="max-w-7xl mx-auto px-5 pt-6">
+        <div className="max-w-7xl mx-auto px-5 pb-8 pt-6">
             <div className="flex items-center">
                 <svg onClick={onBack} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 cursor-pointer">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
