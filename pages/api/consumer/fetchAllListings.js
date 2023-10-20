a4437cc09cc4429249c03dc1ba6992d9b49ce989
Brian-Garbs <93226711+Brian-Garbs@users.noreply.github.com>
2023-10-20 12:10:35 -0600

    Add: wbs 2.1

---
+++ pages/api/consumer/fetchAllListings.js
@@ -0,0 +1,16 @@
+import { prisma } from "../../../db/prismaDB";
+
+const handler = async (req, res) => {
+  try {
+    const listings = await prisma.listing.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    res.status(200).json(listings);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export default handler;
