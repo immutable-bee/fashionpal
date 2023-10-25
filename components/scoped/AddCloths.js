4a646a2a32fddb4e7355e40ec219631f45e66fa6
theibrahim <iamibrahimmemon@gmail.com>
2023-10-25 17:14:17 +0300

    fix

--- components/scoped/AddCloths.js
+++ components/scoped/AddCloths.js
@@ -932,26 +932,13 @@ function ImageUploader({ onBack, onFecth }) {
                       value={defaultPriceSuggestion}
                       type="number"
                       className="w-full mt-1 rounded-xl pl-6 pr-2  py-2 border border-gray-600"
-                      onChange={(e) => setMaxPrice(e.target.value)}
+                      onChange={(e) =>
+                        setDefaultPriceSuggestion(e.target.value)
+                      }
                     />
                   </div>
 
                   <div className="max-w-fit mx-auto mt-5">
-                    <label className="relative mb-4 flex items-center cursor-pointer">
-                      <input
-                        type="checkbox"
-                        value=""
-                        className="sr-only peer"
-                        checked={isOptionToAuction}
-                        onChange={() =>
-                          setIsOptionToAuction(!isOptionToAuction)
-                        }
-                      />
-                      <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-[#f7895e] dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#FF9C75]"></div>
-                      <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-                        Option to Auction
-                      </span>
-                    </label>
                     <label className="relative mb-4 flex items-center cursor-pointer">
                       <input
                         type="checkbox"
