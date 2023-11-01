513bb97c7a32682141518780aad93cfce81e6f23
Brian-Garbs <93226711+Brian-Garbs@users.noreply.github.com>
2023-11-01 15:06:59 -0600

    update: fetch user api route

---
+++ pages/api/user/fetch.js
@@ -0,0 +1,24 @@
+import { prisma } from "../../../db/prismaDB";
+
+const handler = async (req, res) => {
+  const email = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+      include: {
+        consumer: true,
+        business: true,
+      },
+    });
+
+    res.status(200).json(user);
+  } catch (error) {
+    notify.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export default handler;
