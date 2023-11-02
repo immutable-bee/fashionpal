0259bb29e446a188ae0bce86615a84584e6183d8
theibrahim <iamibrahimmemon@gmail.com>
2023-11-02 21:41:25 +0500

    added loading and disabled button states in simple, employee and admin listing, if fetching something then disabling next button, Improved photo capture, when capture displaying what image captured in the meantime of uploading, used next/image and fixed blur image issue, moved big loader to small button loading

--- components/utility/Capture.js
+++ components/utility/Capture.js
@@ -1,11 +1,20 @@
 import React, { useState, useRef, useEffect } from "react";
 import Webcam from "react-webcam";
 import ButtonComponent from "@/components/utility/Button";
+import Image from "next/image";
+import LoadingComponent from "@/components/utility/loading";
+
 import { NotificationManager } from "react-notifications";
 
 function Capture({ text, onCapture, skip = false, loading = false }) {
   const webcamRef = useRef(null);
   const [facingMode, setFacingMode] = useState("environment");
+  const [capturedImage, setCapturedImage] = useState("");
+
+  const capture = () => {
+    setCapturedImage(webcamRef.current.getScreenshot());
+    onCapture(webcamRef.current.getScreenshot());
+  };
 
   useEffect(() => {
     navigator.mediaDevices
@@ -18,34 +27,49 @@ function Capture({ text, onCapture, skip = false, loading = false }) {
 
   return (
     <div>
-      <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        videoConstraints={{
-          facingMode: facingMode,
-        }}
-      />
+      {loading ? (
+        <Image
+          src={capturedImage}
+          width={400}
+          height={400}
+          alt=""
+        />
+      ) : (
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          videoConstraints={{
+            facingMode: facingMode,
+          }}
+        />
+      )}
       <div className="flex gap-3 sm:justify-start justify-center">
-        <ButtonComponent
-          loading={loading}
-          color="grey"
-          className="bg-gray-300 px-5 py-2 rounded-lg mt-3 w-[70%] sm:w-auto"
-          onClick={() => onCapture(webcamRef.current.getScreenshot())}
-        >
-          Capture {text}
-        </ButtonComponent>
-        {skip ? (
-          <ButtonComponent
-            loading={loading}
-            color="grey"
-            className="bg-gray-300 px-5 py-2 rounded-lg mt-3 w-[70%] sm:w-auto"
-            onClick={() => onCapture(null)}
-          >
-            Skip
-          </ButtonComponent>
+        {!loading ? (
+          <>
+            <ButtonComponent
+              loading={loading}
+              color="grey"
+              className="bg-gray-300 px-5 py-2 rounded-lg mt-3 w-[70%] sm:w-auto"
+              onClick={() => capture()}
+            >
+              Capture {text}
+            </ButtonComponent>
+            {skip ? (
+              <ButtonComponent
+                loading={loading}
+                color="grey"
+                className="bg-gray-300 px-5 py-2 rounded-lg mt-3 w-[70%] sm:w-auto"
+                onClick={() => onCapture(null)}
+              >
+                Skip
+              </ButtonComponent>
+            ) : (
+              ""
+            )}
+          </>
         ) : (
-          ""
+          <LoadingComponent />
         )}
       </div>
     </div>
