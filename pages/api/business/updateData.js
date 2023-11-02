e4f4a0899b2b82b6059fc8b84d10c44bcf7678a5
Brian-Garbs <93226711+Brian-Garbs@users.noreply.github.com>
2023-11-02 16:57:40 -0600

    add: update business/consumer routes

---
+++ pages/api/business/updateData.js
@@ -0,0 +1,35 @@
+import { prisma } from "../../../db/prismaDB";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../auth/[...nextauth]";
+
+const handler = async (req, res) => {
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const data = req.body;
+
+  const business = await prisma.business.findUnique({
+    where: { email: session.user.email },
+  });
+
+  if (!business) {
+    return res.status(404).json({ message: "Business record not found" });
+  }
+
+  try {
+    const updatedBusiness = await prisma.business.update({
+      where: { email: session.user.email },
+      data: { ...data },
+    });
+
+    return res
+      .status(200)
+      .json({ message: "Business record updatesd: ", updatedBusiness });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export default handler;
