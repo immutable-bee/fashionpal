fb052134d11a0747261fd2cccadc6ac705cbc9b6
Brian-Garbs <93226711+Brian-Garbs@users.noreply.github.com>
2023-11-07 13:33:00 -0700

    update: prisma query to match schema update

--- pages/api/business/fetchStats.js
+++ pages/api/business/fetchStats.js
@@ -24,7 +24,7 @@ const handler = async (req, res) => {
             },
           },
           select: {
-            isDisposed: true,
+            status: true,
             isActive: true,
             votes: {
               select: {
