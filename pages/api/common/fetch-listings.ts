d75f770c7618aad7749d47592726597f4f32d93a
Paulo Ouriques <ourixilva@gmail.com>
2023-11-21 13:33:29 -0300

    chore: keed listings and images when deleting.

--- pages/api/common/fetch-listings.ts
+++ pages/api/common/fetch-listings.ts
@@ -57,6 +57,7 @@ export default async function handler(
         });
         whereClause.businessId = business?.id;
       }
+      whereClause.isActive = true;
       const listingsWithTags = await prisma.listing.findMany({
         skip,
         take: limit,
