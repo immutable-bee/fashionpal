628b10cab118f475bbd41f752720f1634b2ad5ac
Brian-Garbs <93226711+Brian-Garbs@users.noreply.github.com>
2023-12-08 00:28:47 -0700

    update: add price param

--- components/business/PrintBarcode.js
+++ components/business/PrintBarcode.js
@@ -1,11 +1,12 @@
 import Barcode from "react-barcode";
 
-const PrintBarcode = ({ sku }) => {
+const PrintBarcode = ({ sku, price }) => {
   return (
     <div className="flex flex-col items-center container mx-auto p-4">
       <div className="border-[5px] border-gray-700 rounded-3xl px-8 py-2">
-        <div id="barcode-to-print" className="print:barcode-container">
-          <Barcode value={sku} />
+        <div id="barcode-to-print" className="print:barcode-container flex">
+          <div className="price-text ml-2">{`$${price}`}</div>
+          <Barcode width={1} height={25} value={sku} fontSize={10} />
         </div>
       </div>
 
@@ -18,19 +19,35 @@ const PrintBarcode = ({ sku }) => {
 
       <style>
         {`
+          .price-text {
+            margin-left: 8px;
+            font-size: 16px;
+            align-self: center;
+            writing-mode: vertical-rl;
+            transform: rotate(180deg);
+            font-size: 20px;
+            white-space: nowrap;
+          }
+
           @media print {
             body * {
               visibility: hidden;
             }
             #barcode-to-print, #barcode-to-print * {
               visibility: visible;
+
             }
             .print\\:barcode-container {
               position: fixed;
               top: 50%;
               left: 50%;
               transform: translate(-50%, -50%);
+              width: 57mm;
+              height: 32mm;
+              display: flex;
+              align-items: center;
             }
+
           }
         `}
       </style>
