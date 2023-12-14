09075f1034f841d4c2d17df406e187585d6595c4
immutable-bee <immutablebee@gmail.com>
2023-12-14 16:10:06 -0700

    update: print templates/styles

--- components/business/PrintBarcode.js
+++ components/business/PrintBarcode.js
@@ -1,113 +1,145 @@
+import Script from "next/script";
 import Barcode from "react-barcode";
 import { QRCode } from "react-qrcode-logo";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const PrintBarcode = ({ sku, price }) => {
-  const [template, setTemplate] = useState("1");
+  const [template, setTemplate] = useState(1);
+
+  const printDymoLabel = () => {
+    const dymoLabel = createDymoLabel(template, {
+      PRICE_VALUE: price,
+      URL_VALUE: sku,
+      BARCODE_VALUE: sku,
+    });
+    dymo.print("DYMO LabelWriter 450", dymoLabel);
+  };
 
   return (
-    <div className="flex flex-col items-center container mx-auto p-4 w-full">
-      <div className="border-[5px] w-full max-w-xs border-gray-700 rounded-3xl px-3 py-4">
-        <div
-          id="barcode-to-print"
-          className="print:barcode-container flex justify-center"
-        >
-          {template === "1" && (
-            <>
-              <div className="w-1/2 border-r-2 border-black flex flex-col items-center">
-                <h6 className=" text-center text-sm">Members Price</h6>
-                <QRCode
-                  value={`$${price}`}
-                  size={80}
-                />
+    <>
+      <div className="flex flex-col items-center container mx-auto p-4">
+        <div className="border-[5px] border-gray-700 rounded-3xl px-8 py-4">
+          <div id="barcode-to-print" className="print:barcode-container">
+            {template === "1" && (
+              <div className="print:qr-container flex flex-col items-center">
+                <h6 className="price-text">Sku</h6>
+                <QRCode value={`$${sku}`} size={75} />
               </div>
-              {/* <div className="ml-2 border-l-2 border-black"></div> */}
-              <div className="w-1/2 overflow-hidden  flex flex-col items-center justify-center">
-                <div className="mr-3">{`$${price}`}</div>
-                <Barcode
-                  width={1}
-                  height={25}
-                  value={sku}
-                  fontSize={10}
-                />
+            )}
+            {template === "2" && (
+              <div className="print:qr-container flex flex-col items-center">
+                <h6 className="price-text">Members Price</h6>
+                <QRCode value={`$${price}`} size={75} />
               </div>
-            </>
-          )}
-          {template === "2" && (
-            <>
-              <div className="price-text tp-2 mb-2 ">{`$${price}`}</div>
-              <Barcode
-                width={1}
-                height={25}
-                value={sku}
-                fontSize={10}
-              />
-            </>
-          )}
-          {template === "3" && (
-            <>
-              <h6 className="price-text">Members Price</h6>
-              <QRCode value={`$${price}`} />
-            </>
-          )}
+            )}
+            {template === "3" && (
+              <div className="w-full h-full flex flex-col items-center">
+                <div className="price-text ">{`$${price}`}</div>
+                <div id="barcode-2-container">
+                  <Barcode width={1.3} height={40} value={sku} fontSize={10} />
+                </div>
+              </div>
+            )}
+            {template === "4" && (
+              <div className="flex flex-col items-center">
+                <h6 className="price-text">Members Price</h6>
+                <QRCode value={`$${price}`} size={60} />
+              </div>
+            )}
+            {template === "5" && (
+              <div className="flex">
+                <div className="flex flex-col items-center">
+                  <h6 className="w-2/3 text-center text-sm">Members Price</h6>
+                  <QRCode value={`$${price}`} size={75} />
+                </div>
+                <div className="ml-2 border-l-2 border-black"></div>
+                <div className="flex flex-col items-center justify-center">
+                  <div className="mr-3">{`$${price}`}</div>
+                  <Barcode width={1} height={25} value={sku} fontSize={10} />
+                </div>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
 
-      <button
-        onClick={() => window.print()}
-        className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700"
-      >
-        Print Barcode
-      </button>
+        <button
+          onClick={() => window.print()}
+          className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+        >
+          Print Label
+        </button>
 
-      <label className="mt-5 text-lg">Template</label>
-      <select
-        value={template}
-        className="mt-1 rounded-xl px-3 py-2 border border-gray-600"
-        onChange={(e) => setTemplate(e.target.value)}
-      >
-        <option value={"1"}>QR & Barcode</option>
-        <option value={"2"}>Barcode Only</option>
-        <option value={"3"}>QR Code Only</option>
-      </select>
+        <label className="mt-5 text-lg">Template</label>
+        <select
+          value={template}
+          className="mt-1 rounded-xl px-3 py-2 border border-gray-600"
+          onChange={(e) => setTemplate(e.target.value)}
+        >
+          <option value={"1"}>QR Code (Sku) 1x1</option>
+          <option value={"2"}>QR Code (Product Link) 1x1</option>
+          <option value={"3"}>Barcode (Sku) 1.25 x 2.25</option>
+          <option value={"4"}>QR Code (Product Link) 1.25 x 2.25</option>
+          <option value={"5"}>Barcode & QR Code 4 x 6</option>
+        </select>
 
-      <style>
-        {`
+        <style>
+          {`
           .price-text {
-            font-size: 16px;
-            align-self: center;
-            writing-mode: vertical-rl;
-            transform: rotate(180deg);
-            font-size: 20px;
+            font-size: 14px;
             white-space: nowrap;
           }
           @media print {
+            header, footer, aside, form {
+              display: none;
+            }
+
             body * {
               visibility: hidden;
             }
             #barcode-to-print, #barcode-to-print * {
               visibility: visible;
+            }
+            @page {
+              margin-top: 15px;
+
+              size: auto;
 
             }
+            html, body {
+              height: 100vh;
+              overflow: hidden;
+              margin: 0;
+              padding: 0;
+            }
+
             .print\\:barcode-container {
-                position: fixed;
-                top: 50%;
-                left: 50%;
-                transform: translate(-50%, -50%);
-                width: 57mm;
-                height: 32mm;
-                display: flex;
-                align-items: center;
-                justify-content: evenly; horizontally
-              }
-            .print\\:barcode-container > div:nth-child(2) {
-                border-left: 2px solid black;
-                height: 30mm;
-                margin: 0 4mm;
+              position: absolute;
+              max-width: 100vw;
+              max-height: 100vh;
+              top: 0;
+              left: 25px;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+            }
+
+            .print\\:qr-container {
+              margin-left: 10px
+            }
+
+            #barcode-2-container {
+              width: 100%;
+            }
+
+            .print\\:barcode-container > div {
+              max-width: 100%;
+              max-height: 100%;
+            }
           }
         `}
-      </style>
-    </div>
+        </style>
+      </div>
+    </>
   );
 };
 
