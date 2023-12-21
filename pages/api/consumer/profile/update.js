718d269233e7908d9bc342bbce9ecf7e694df596
bilal <bilal.awan@toptal.com>
2023-12-22 00:49:29 +0500

    profile page consumer front end and migration issue fixed

---
+++ pages/api/consumer/profile/update.js
@@ -0,0 +1,36 @@
+
+\import { getServerSession } from "next-auth";
+import { prisma } from "../../../../db/prismaDB";
+import { authOptions } from "../../auth/[...nextauth]";
+
+const handler = async (req, res) => {
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const data = req.body;
+
+  const consumer = await prisma.consumer.findUnique({
+    where: { email: session.user.email },
+  });
+
+  if (!consumer) {
+    return res.status(404).json({ message: "Consumer record not found" });
+  }
+
+  try {
+    const updatedConsumer = await prisma.consumer.update({
+      where: { email: session.user.email },
+      data: { ...data },
+    });
+
+    return res
+      .status(200)
+      .json({ message: "Consumer record updatesd: ", updatedConsumer });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export default handler;
