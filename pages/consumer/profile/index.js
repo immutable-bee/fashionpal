7af76b0eddf359cd7b434cd2132d23e9fef98d0d
bilal <bilal.awan@toptal.com>
2023-12-23 02:51:22 +0500

    fixed the file path issue

--- pages/consumer/profile/index.js
+++ pages/consumer/profile/index.js
@@ -25,7 +25,6 @@ const ProfileComponent = ({}) => {
     } else {
       const data = await response.json();
       setConsumerData(data);
-      console.log(data);
     }
   };
   useEffect(() => {
