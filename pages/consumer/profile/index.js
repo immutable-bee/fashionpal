b51f1ba2fb7c750a50e17827059d2a57346d4add
bilal <bilal.awan@toptal.com>
2023-12-27 01:16:25 +0500

    changes applied plus add new thrift item functionality added

--- pages/consumer/profile/index.js
+++ pages/consumer/profile/index.js
@@ -5,7 +5,6 @@ import { signOut } from "next-auth/react";
 import { QRCode } from "react-qrcode-logo";
 
 import ConsumerInfo from "@/components/consumer/profile/ConsumerInfo";
-// import ThriftList from "../../../components/consumer/profile/thriftList";
 import ThriftList from "@/components/consumer/profile/Thrifts";
 
 const ProfileComponent = ({}) => {
