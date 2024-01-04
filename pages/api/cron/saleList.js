3bea49207d2e69426cf4c5492430096ae687f4e7
bilal <bilal.awan@toptal.com>
2024-01-05 02:29:06 +0500

    update the logic for calculate sale price

--- pages/api/cron/saleList.js
+++ pages/api/cron/saleList.js
@@ -7,47 +7,34 @@ export const config = {
   },
 };
 function calculateDiscount(listing, pricingRule) {
-  console.table(pricingRule);
-  // Logic to calculate the difference in weeks
   const createdDate = new Date(listing.createdAt);
-  console.log(createdDate);
   const currentDate = new Date();
-  console.log(currentDate);
   const timeDifference = currentDate - createdDate;
   let cycleInMilliseconds;
   if (pricingRule.cycle === "weekly") {
-    console.log("Weekly");
     cycleInMilliseconds = 7 * 24 * 60 * 60 * 1000;
   } else if (pricingRule.cycle === "bi-weekly") {
-    console.log("bi-weekly");
     cycleInMilliseconds = 2 * 7 * 24 * 60 * 60 * 1000;
   } else if (pricingRule.cycle === "monthly") {
-    console.log("monthly");
     cycleInMilliseconds = 30 * 24 * 60 * 60 * 1000;
   } else {
     return null;
   }
 
   const cycleDifference = Math.floor(timeDifference / cycleInMilliseconds);
-  console.log(pricingRule.cycle, cycleDifference);
   let discountPercentage = (cycleDifference + 1) * pricingRule.adjustPriceBy;
 
-  console.log(listing.price, pricingRule.adjustPriceBy);
   const calculation =
     listing.price - (discountPercentage / 100) * listing.price;
-  console.log(pricingRule.floorPrice);
   let discountPrice =
     calculation > pricingRule.floorPrice ? calculation : pricingRule.floorPrice;
   discountPrice = Number.isInteger(discountPrice)
     ? discountPrice - pricingRule.roundTo
     : discountPrice;
-  console.log("calculation", calculation);
-  console.log("discountPrice", discountPrice);
   return {
     discountPrice,
     discountPercentage,
   };
-  // }
 }
 
 const handler = async (req, res) => {
@@ -62,39 +49,40 @@ const handler = async (req, res) => {
       },
     });
     const pricingRules = await prisma.PricingRule.findMany({});
+    const pricingRuleMap = new Map();
+    pricingRules.forEach((rule) => {
+      const key = `${rule.categoryId}-${rule.ownerId}`;
+      pricingRuleMap.set(key, rule);
+    });
     const updatedListings = [];
-    const discountedListings = listings.map(async (listing) => {
-      const applicablePricingRule = pricingRules.find(
-        (rule) =>
-          !!(
-            listing.categories.length > 0 &&
-            listing.categories[0].categoryId === rule.categoryId &&
-            rule.ownerId === listing.businessId
-          )
-      );
-      console.log(applicablePricingRule);
-      if (applicablePricingRule) {
-        const discountInfo = calculateDiscount(listing, applicablePricingRule);
-        console.log(discountInfo);
+    listings.map(async (listing) => {
+      if (listing.categories.length > 0) {
+        const key = `${listing.categories[0].categoryId}-${listing.businessId}`;
+        const applicablePricingRule = pricingRuleMap.get(key);
+        if (applicablePricingRule) {
+          const discountInfo = calculateDiscount(
+            listing,
+            applicablePricingRule
+          );
 
-        if (discountInfo) {
-          // Update the database with the calculated discounts
-          const updatedListing = await prisma.listing.update({
-            where: { id: listing.id },
-            data: {
-              discountPrice: discountInfo.discountPrice,
-              discountPercentage: discountInfo.discountPercentage,
-            },
-          });
+          if (discountInfo) {
+            const updatedListing = await prisma.listing.update({
+              where: { id: listing.id },
+              data: {
+                discountPrice: discountInfo.discountPrice,
+                discountPercentage: discountInfo.discountPercentage,
+              },
+            });
 
-          updatedListings.push(updatedListing);
+            updatedListings.push(updatedListing);
+          }
         }
-      }
 
-      return listing;
+        return listing;
+      }
     });
 
-    res.status(200).json({ message: "Success" });
+    res.status(200).json({ message: "success" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
