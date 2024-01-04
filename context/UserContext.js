82c3c3eefde2dbb79087d6ea755d6957d7bdede6
theibrahim <iamibrahimmemon@gmail.com>
2024-01-04 11:35:21 +0500

    added handler

--- context/UserContext.js
+++ context/UserContext.js
@@ -103,130 +103,131 @@ export const UserProvider = ({ children }) => {
 
   // useEffect to handle user-related logic on component mount and updates
   useEffect(() => {
-    if (router.query.authenticating) {
-      setUserDataFetched(true); // Set to true even on error to prevent infinite loadin
-      return;
-    }
-    console.log(router);
-    if (router.pathname === "/scan/[type.js]/[id]") {
-      setUserDataFetched(true); // Set to true even on error to prevent infinite loadin
-      return;
-    }
-    // Skip logic on initial render and if on error page
-    if (router.asPath !== "/" && router.pathname === "/404") {
-      setUserDataFetched(true); // Set to true even on error to prevent infinite loadin
-      return;
-    }
-    if (isInitialRender.current) {
-      isInitialRender.current = false;
-      return;
-    }
-
-    // If not logged in, not already on the "/auth" page, and not in the process of authentication
-    console.log(2);
-    console.log(session);
-    console.log(router);
-
-    if (!session && router.pathname !== "/auth") {
-      // Redirect to "/auth" page with the current route's pathname and existing query parameters
-      const route = {
-        pathname: "/auth",
-      };
-      if (router.pathname !== "/") {
-        route.query = {};
-        route.query = {
-          redirectUrl: router.pathname,
-        };
+    const handler = async () => {
+      if (router.query.authenticating) {
+        setUserDataFetched(true); // Set to true even on error to prevent infinite loadin
+        return;
       }
-      router.push(route);
-    }
 
-    if (session && router.pathname == "/") {
-      router.push("/business");
-    }
+      if (router.pathname === "/scan/[type.js]/[id]") {
+        setUserDataFetched(true); // Set to true even on error to prevent infinite loadin
+        return;
+      }
+      // Skip logic on initial render and if on error page
+      if (router.asPath !== "/" && router.pathname === "/404") {
+        setUserDataFetched(true); // Set to true even on error to prevent infinite loadin
+        return;
+      }
+      if (isInitialRender.current) {
+        isInitialRender.current = false;
+        return;
+      }
 
-    // If the user is logged in
-    if (session) {
-      // Fetch user data, including business data if applicable
-      // fetchUserData();
-      fetchUserData()
-        .then(() => setUserDataFetched(true))
-        .catch((error) => {
-          console.error("Error fetching user data:", error);
-          setUserDataFetched(true); // Set to true even on error to prevent infinite loading
-        });
+      // If not logged in, not already on the "/auth" page, and not in the process of authentication
 
-      // If the user is not yet onboarded
-      if (user && !user.onboardingComplete) {
-        if (router.pathname === "/auth/onboarding") {
-          return;
-        }
-        // Define allowed routes based on the user role
-        const allowedRoutes = ACCESS_RULES.business || [];
-
-        // Define the route object for redirection
+      if (!session && router.pathname !== "/auth") {
+        // Redirect to "/auth" page with the current route's pathname and existing query parameters
         const route = {
-          pathname: "/auth/onboarding",
+          pathname: "/auth",
         };
-
-        // Determine the proper redirect URL for onboarding
-        if (
-          router.query &&
-          router.query.redirectUrl &&
-          router.query.redirectUrl !== "/auth/onboarding" &&
-          allowedRoutes.find((route) => router.query.redirectUrl === route)
-        ) {
-          // If conditions are met, set the redirectUrl in the route's query parameters
+        if (router.pathname !== "/") {
           route.query = {};
-          route.query.redirectUrl = router.query.redirectUrl;
-        } else if (
-          // If no valid redirectUrl in query parameters, check if the current route is allowed
-          router.asPath !== "/auth/onboarding" &&
-          allowedRoutes.find((route) => router.asPath === route)
-        ) {
-          // If the current route is allowed, set it as the redirectUrl
-          route.query = {};
-          route.query.redirectUrl = router.pathname;
+          route.query = {
+            redirectUrl: router.pathname,
+          };
         }
-
-        // Redirect the user to the determined route
         router.push(route);
       }
-      console.log(user);
-      // Determine the user role
-      const role = user?.consumer
-        ? "consumer"
-        : user?.business
-        ? "business"
-        : null;
 
-      console.log(role);
-      const allowedRoutes = ACCESS_RULES[role] || [];
-
-      // Check if the user role is defined and the router pathname is not in allowedRoutes
-      if (role && !allowedRoutes.find((route) => router.pathname === route)) {
-        // Redirect the user to the first allowed route if not on the "/auth" page
-        if (
-          !router.query.redirectUrl ||
-          router.pathname !== "/auth" ||
-          (router.pathname === "/auth" &&
-            router.query.redirectUrl &&
-            !allowedRoutes.find((route) => router.query.redirectUrl === route))
-        ) {
-          // Redirect to the first allowed route
-          router.push({
-            pathname: allowedRoutes[0],
-          });
-        } else {
-          // Redirect the user to the specified redirect URL
-          router.push({
-            pathname: router.query.redirectUrl,
-          });
-        }
+      if (session && router.pathname == "/") {
+        router.push("/business");
       }
-    } else {
-      setUserDataFetched(true); // Set to true even on error to prevent infinite loadin
-    }
+
+      // If the user is logged in
+      if (session) {
+        // Fetch user data, including business data if applicable
+        // fetchUserData();
+        fetchUserData()
+          .then(() => setUserDataFetched(true))
+          .catch((error) => {
+            console.error("Error fetching user data:", error);
+            setUserDataFetched(true); // Set to true even on error to prevent infinite loading
+          });
+
+        // If the user is not yet onboarded
+        if (user && !user.onboardingComplete) {
+          if (router.pathname === "/auth/onboarding") {
+            return;
+          }
+          // Define allowed routes based on the user role
+          const allowedRoutes = ACCESS_RULES.business || [];
+
+          // Define the route object for redirection
+          const route = {
+            pathname: "/auth/onboarding",
+          };
+
+          // Determine the proper redirect URL for onboarding
+          if (
+            router.query &&
+            router.query.redirectUrl &&
+            router.query.redirectUrl !== "/auth/onboarding" &&
+            allowedRoutes.find((route) => router.query.redirectUrl === route)
+          ) {
+            // If conditions are met, set the redirectUrl in the route's query parameters
+            route.query = {};
+            route.query.redirectUrl = router.query.redirectUrl;
+          } else if (
+            // If no valid redirectUrl in query parameters, check if the current route is allowed
+            router.asPath !== "/auth/onboarding" &&
+            allowedRoutes.find((route) => router.asPath === route)
+          ) {
+            // If the current route is allowed, set it as the redirectUrl
+            route.query = {};
+            route.query.redirectUrl = router.pathname;
+          }
+
+          // Redirect the user to the determined route
+          router.push(route);
+        }
+
+        // Determine the user role
+        const role = user?.consumer
+          ? "consumer"
+          : user?.business
+          ? "business"
+          : null;
+
+        const allowedRoutes = ACCESS_RULES[role] || [];
+
+        // Check if the user role is defined and the router pathname is not in allowedRoutes
+        if (role && !allowedRoutes.find((route) => router.pathname === route)) {
+          // Redirect the user to the first allowed route if not on the "/auth" page
+          if (
+            !router.query.redirectUrl ||
+            router.pathname !== "/auth" ||
+            (router.pathname === "/auth" &&
+              router.query.redirectUrl &&
+              !allowedRoutes.find(
+                (route) => router.query.redirectUrl === route
+              ))
+          ) {
+            // Redirect to the first allowed route
+            router.push({
+              pathname: allowedRoutes[0],
+            });
+          } else {
+            // Redirect the user to the specified redirect URL
+            router.push({
+              pathname: router.query.redirectUrl,
+            });
+          }
+        }
+      } else {
+        setUserDataFetched(true); // Set to true even on error to prevent infinite loadin
+      }
+    };
+    handler();
   }, [session, user?.onboardingComplete, router.pathname]);
 
   // Provide the user context to the components
