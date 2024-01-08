2ee12a6c14bd61ff3d2734b54085aa21e4321341
theibrahim <iamibrahimmemon@gmail.com>
2024-01-08 16:35:09 +0500

    business profile page api and square input....

--- components/scoped/EditBusinessProfileModal.js
+++ components/scoped/EditBusinessProfileModal.js
@@ -41,11 +41,14 @@ const Modal = ({ onClose, onDone }) => {
   };
 
   const onSave = () => {
-    onDone();
+    const JSON = {
+      name,
+      email,
+      data: clothingValues,
+    };
 
-    console.log(name);
-    console.log(email);
-    console.log(clothingValues);
+    console.log(JSON);
+    onDone();
   };
 
   return (
@@ -108,47 +111,52 @@ const Modal = ({ onClose, onDone }) => {
               Add Category
             </button>
           </div>
-
-          <div className="flex items-center justify-end  mt-3">
-            <h1 className="pr-[36px] font-medium">Days</h1>
-          </div>
-          {/* map */}
-          {categories.map((category) => (
-            <div
-              key={category}
-              className="flex items-center justify-between mt-2"
-            >
-              <div className="flex items-center">
-                <svg
-                  onClick={() => deleteCategory(category)}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-4 h-4 cursor-pointer hover:opacity-70 text-orange-500 mr-[10px]"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                  />
-                </svg>
-
-                <h1 className="pr-2 font-medium">{category}</h1>
+          {categories && categories.length !== 0 ? (
+            <div>
+              <div className="flex items-center justify-end  mt-3">
+                <h1 className="pr-[36px] font-medium">Days</h1>
               </div>
-              <input
-                value={clothingValues[category]}
-                onChange={(e) =>
-                  setClothingValues({
-                    ...clothingValues,
-                    [category]: e.target.value,
-                  })
-                }
-                className="font-medium px-2 py-1 border focus:ring-1 focus:ring-[#ffc71f] border-green-500 focus:outline-none text-black rounded-xl w-28 h-10"
-              />
+              {/* map */}
+              {categories.map((category) => (
+                <div
+                  key={category}
+                  className="flex items-center justify-between mt-2"
+                >
+                  <div className="flex items-center">
+                    <svg
+                      onClick={() => deleteCategory(category)}
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-4 h-4 cursor-pointer hover:opacity-70 text-orange-500 mr-[10px]"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+
+                    <h1 className="pr-2 font-medium">{category}</h1>
+                  </div>
+                  <input
+                    value={clothingValues[category]}
+                    onChange={(e) =>
+                      setClothingValues({
+                        ...clothingValues,
+                        [category]: e.target.value,
+                      })
+                    }
+                    className="font-medium px-2 py-1 border focus:ring-1 focus:ring-[#ffc71f] border-green-500 focus:outline-none text-black rounded-xl w-28 h-10"
+                  />
+                </div>
+              ))}
             </div>
-          ))}
+          ) : (
+            <h3 className="text-center mt-4 text-xl">No categories added</h3>
+          )}
         </div>
       </div>
     </ModalComponent>
--- components/scoped/ProfileComponent.js
+++ components/scoped/ProfileComponent.js
@@ -1,19 +1,69 @@
-// App.js
 import EditBusinessProfileModal from "./EditBusinessProfileModal";
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import ButtonComponent from "@/components/utility/Button";
+import Loading from "@/components/utility/loading";
 function App() {
   const [editModal, setEditModal] = useState(false);
-  const [editName, setEditName] = useState("");
-  const [email, setEmail] = useState("");
-  const [priority, setPriority] = useState(false);
   const [isWeeklyEmailReports, setisWeeklyEmailReports] = useState(false);
   const [daysThroughTraget, setDaysThroughTarget] = useState("1750");
   const [daysASPTarget, setDaysASPTarget] = useState("$12");
+  const [fetchingUser, setFetchingUser] = useState(true);
+  const [businessData, setBusinessData] = useState({});
+  const [updating, setUpdating] = useState(false);
 
-  const handleStarClick = () => {
-    setPriority(!priority);
+  useEffect(() => {
+    fetchBusinessData().then();
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setBusinessData({ ...businessData, [name]: value });
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!businessData.businessName) {
+      NotificationManager.error("Store name is required!");
+      return;
+    }
+    setUpdating(true);
+    try {
+      await fetch("/api/business/updateData", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: businessData.email,
+          data: {
+            businessName: businessData.businessName,
+            squareAccessToken: businessData.squareAccessToken,
+          },
+        }),
+      });
+      await fetchBusinessData();
+    } catch (error) {}
+    setUpdating(false);
+  };
+
+  const fetchBusinessData = async () => {
+    setFetchingUser(true);
+    const response = await fetch(`/api/user/fetch`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    setFetchingUser(false);
+    if (response.ok) {
+      setBusinessData(data.business);
+    } else {
+      return console.error("Failed to fetch user data:", data.error);
+    }
+  };
+
   const handleIsWeeklyEmailReports = () => {
     setisWeeklyEmailReports(!isWeeklyEmailReports);
   };
@@ -23,14 +73,7 @@ function App() {
   const onClose = () => {
     setEditModal(!editModal);
   };
-  const handleStoreName = (e) => {
-    setEditName(e.target.value);
-    console.log(editName);
-  };
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-    console.log(email);
-  };
+
   const handleDaysThroughTarget = (e) => {
     setDaysThroughTarget(e.target.value);
     console.log(daysThroughTraget);
@@ -44,27 +87,66 @@ function App() {
     <div className="my-5 flex justify-center ">
       <div className="sm:w-[420px]">
         <div>
-          <div>
-            <h1 className="text-md text-gray-700">Store Name</h1>
-            <input
-              value={editName}
-              onChange={handleStoreName}
-              className="bg-white focus:ring-1 focus:ring-[#ffc71f] focus:outline-none form-input border border-gray-500 w-full rounded-lg  px-4 my-1 py-2"
-            />
-          </div>
-          <div className="mt-3">
-            <h1 className="text-md text-gray-700">Email</h1>
-            <input
-              value={email}
-              onChange={handleEmail}
-              className="bg-white focus:ring-1 focus:ring-[#ffc71f] focus:outline-none form-input border border-gray-500 w-full rounded-lg  px-4 my-1 py-2"
-            />
-          </div>
+          {fetchingUser ? (
+            <div>
+              <div>
+                <div className="h-[312px] flex justify-center items-center">
+                  <Loading size="xl" />
+                </div>
+              </div>
+            </div>
+          ) : (
+            <form onSubmit={handleSubmit}>
+              <div className="py-2">
+                <label className="text-sm text-gray-700">Store name</label>
+                <input
+                  value={businessData?.businessName}
+                  name="businessName"
+                  type="text"
+                  className="bg-white focus:ring-1 focus:ring-[#ffc71f] focus:outline-none form-input border border-gray-500 w-full rounded-lg  px-4 my-1 py-2"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="py-2">
+                <label className="text-sm text-gray-700">Email</label>
+                <input
+                  disabled={true}
+                  value={businessData?.email}
+                  name="email"
+                  type="text"
+                  className="bg-gray-100 focus:ring-1 focus:ring-[#ffc71f] focus:outline-none form-input border border-gray-300 w-full rounded-lg  px-4 my-1 py-2"
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="py-2">
+                <label className="text-sm text-gray-700">
+                  Square Access Token
+                </label>
+                <input
+                  value={businessData?.squareAccessToken}
+                  name="squareAccessToken"
+                  type="password"
+                  className="bg-white focus:ring-1 focus:ring-[#ffc71f] focus:outline-none form-input border border-gray-500 w-full rounded-lg  px-4 my-1 py-2"
+                  onChange={handleChange}
+                />
+              </div>
+              <ButtonComponent
+                className="mt-3"
+                rounded
+                full
+                loading={updating}
+                type="submit"
+              >
+                Update
+              </ButtonComponent>
+            </form>
+          )}
           <div className="mt-4 flex items-center justify-between">
-            <div className="">
+            <div>
               <h1 className="text-md text-gray-700">Weekly Email Reports </h1>
             </div>
-            <div className="">
+            <div>
               <div className="h-9 flex items-center">
                 <label className="relative w-12 inline-flex items-center cursor-pointer mx-1">
                   <input
@@ -88,9 +170,9 @@ function App() {
           </div>
           <div className="mt-4">
             <h1 className="text-xl">Key Performance Metrics</h1>
-            <div className="flex rounded-2xl mt-2 pb-3 pt-1 pl-3 pr-1 border shadow-sm">
-              <div className="w-[94%] mt-2">
-                <div className="flex items-center justify-between mt-3">
+            <div className=" rounded-2xl mt-2 py-3 px-3 border shadow-sm">
+              <div className="w-full ">
+                <div className="flex items-center justify-between">
                   <h1 className="text-md text-gray-700">
                     90 day AVG Listings added:
                   </h1>
@@ -112,50 +194,17 @@ function App() {
                   </h1>
                   <input
                     value={daysThroughTraget}
+                    type="number"
                     onChange={handleDaysThroughTarget}
                     className="font-medium px-2 py-1 border focus:ring-1 focus:ring-[#ffc71f] border-green-500 focus:outline-none text-black rounded-xl w-28 h-10"
                   />
                 </div>
               </div>
-              <div className="flex justify-end pl-3">
-                <input
-                  type="checkbox"
-                  className="sr-only peer"
-                  value={priority}
-                  data-gtm-form-interact-field-id="0"
-                />
-                <svg
-                  onClick={handleStarClick}
-                  xmlns="http://www.w3.org/2000/svg"
-                  className={`icon icon-tabler icon-tabler-star-filled mt-0.5 mr-0.5 cursor-pointer ${
-                    priority ? "text-yellow-500" : "text-gray-500"
-                  }`}
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  stroke-width="2"
-                  stroke="currentColor"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path
-                    stroke="none"
-                    d="M0 0h24v24H0z"
-                    fill="none"
-                  />
-                  <path
-                    d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z"
-                    stroke-width="0"
-                    fill="currentColor"
-                  />
-                </svg>
-              </div>
             </div>
           </div>
           <div className="mt-1">
             <div className=" rounded-2xl mt-2 py-3 px-3 border shadow-sm">
-              <div className="w-[94%]">
+              <div className="w-full">
                 <div className="flex items-center justify-between">
                   <h1 className="text-md text-gray-700">90 day ALP:</h1>
                   <h3 className="flex items-center justify-between font-medium px-2 py-1 border border-gray-950 text-black rounded-xl w-28 h-10">
@@ -172,6 +221,7 @@ function App() {
                   <h1 className="text-md text-gray-700">90 day ASP Target:</h1>
                   <input
                     value={daysASPTarget}
+                    type="number"
                     onChange={handleDaysASPTarget}
                     className="font-medium px-2 focus:ring-1 focus:ring-[#ffc71f] py-1 border border-green-500 focus:outline-none text-black rounded-xl w-28 h-10"
                   />
@@ -198,13 +248,11 @@ function App() {
             </div>
           </div>
         </div>
-        {editModal === true ? (
+        {editModal === true && (
           <EditBusinessProfileModal
             onClose={onClose}
             onDone={onDone}
           />
-        ) : (
-          ""
         )}
       </div>
     </div>
