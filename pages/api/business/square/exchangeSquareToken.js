072652c8bd3445745da6ea1812f8505239366920
immutable-bee <immutablebee@gmail.com>
2024-01-22 14:03:44 -0700

    Add: square token exchange route

---
+++ pages/api/business/square/exchangeSquareToken.js
@@ -0,0 +1,53 @@
+import { prisma } from "../../../../db/prismaDB";
+import { authOptions } from "../../auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+import { AES } from "crypto-ts";
+
+const handler = async (req, res) => {
+  const session = await getServerSession(req, res, authOptions);
+
+  const { code } = req.body;
+
+  const tokenResponse = await fetch(
+    "https://connect.squareup.com/oauth2/token",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        client_id: process.env.SQUARE_APP_ID,
+        client_secret: process.env.SQUARE_SECRET,
+        code: code,
+        grant_type: "authorization_code",
+      }),
+    }
+  );
+
+  const data = await tokenResponse.json();
+
+  if (!tokenResponse.ok) {
+    return res.status(500).json({ error: "Failed to exchange token" });
+  }
+
+  const encryptedAccessToken = AES.encrypt(
+    data.access_token,
+    process.env.NEXTAUTH_SECRET
+  ).toString();
+  const encryptedRefreshToken = AES.encrypt(
+    data.refresh_token,
+    process.env.NEXTAUTH_SECRET
+  ).toString();
+
+  const business = await prisma.business.update({
+    where: { email: session.user.email },
+    data: {
+      squareAccessToken: encryptedAccessToken,
+      squareRefreshToken: encryptedRefreshToken,
+    },
+  });
+
+  res.status(200).json({ message: "Token exchange successful" });
+};
+
+export default handler;
