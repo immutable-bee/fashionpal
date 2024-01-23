84cfcabb86f3d42f515ad04eb9b828360faffd03
immutable-bee <immutablebee@gmail.com>
2024-01-23 12:49:39 -0700

    Add: logging

--- pages/api/business/square/exchangeSquareToken.js
+++ pages/api/business/square/exchangeSquareToken.js
@@ -8,49 +8,53 @@ const handler = async (req, res) => {
 
   const { code } = req.body;
 
-  const tokenResponse = await fetch(
-    "https://connect.squareup.com/oauth2/token",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        client_id: process.env.SQUARE_APP_ID,
-        client_secret: process.env.SQUARE_SECRET,
-        code: code,
-        grant_type: "authorization_code",
-      }),
+  try {
+    const tokenResponse = await fetch(
+      "https://connect.squareup.com/oauth2/token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          client_id: process.env.SQUARE_APP_ID,
+          client_secret: process.env.SQUARE_SECRET,
+          code: code,
+          grant_type: "authorization_code",
+        }),
+      }
+    );
+
+    if (!tokenResponse.ok) {
+      return res.status(500).json({ error: "Failed to exchange token" });
     }
-  );
 
-  if (!tokenResponse.ok) {
-    return res.status(500).json({ error: "Failed to exchange token" });
+    const data = await tokenResponse.json();
+
+    const encryptedAccessToken = AES.encrypt(
+      data.access_token,
+      process.env.NEXTAUTH_SECRET
+    ).toString();
+    const encryptedRefreshToken = AES.encrypt(
+      data.refresh_token,
+      process.env.NEXTAUTH_SECRET
+    ).toString();
+
+    const currentDate = new Date().toISOString();
+
+    const business = await prisma.business.update({
+      where: { email: session.user.email },
+      data: {
+        squareAccessToken: encryptedAccessToken,
+        squareRefreshToken: encryptedRefreshToken,
+        squareTokenIssueDate: currentDate,
+      },
+    });
+
+    res.status(200).json({ message: "Token exchange successful" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-
-  const data = await tokenResponse.json();
-
-  const encryptedAccessToken = AES.encrypt(
-    data.access_token,
-    process.env.NEXTAUTH_SECRET
-  ).toString();
-  const encryptedRefreshToken = AES.encrypt(
-    data.refresh_token,
-    process.env.NEXTAUTH_SECRET
-  ).toString();
-
-  const currentDate = new Date().toISOString();
-
-  const business = await prisma.business.update({
-    where: { email: session.user.email },
-    data: {
-      squareAccessToken: encryptedAccessToken,
-      squareRefreshToken: encryptedRefreshToken,
-      squareTokenIssueDate: currentDate,
-    },
-  });
-
-  res.status(200).json({ message: "Token exchange successful" });
 };
 
 export default handler;
