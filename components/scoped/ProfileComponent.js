8ff1f5d7c40d0910eeb38cbe63d715a11987ffee
immutable-bee <immutablebee@gmail.com>
2024-01-23 12:39:37 -0700

    fix: generate state clientside

--- components/scoped/ProfileComponent.js
+++ components/scoped/ProfileComponent.js
@@ -102,7 +102,11 @@ function ProfileComponent() {
   };
 
   const handleSquareStateCode = () => {
-    const state = crypto.randomBytes(16).toString("hex");
+    const array = new Uint8Array(16);
+    window.crypto.getRandomValues(array);
+    const state = Array.from(array, (byte) =>
+      byte.toString(16).padStart(2, "0")
+    ).join("");
     setSquareStateCode(state);
     return state;
   };
