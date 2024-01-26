8a4eeb31ba23a7c16278df229c6ea1e1efb3211c
immutable-bee <immutablebee@gmail.com>
2024-01-26 16:11:33 -0700

    Refactor: auto follow and redirect

--- pages/consumer/follow-business.js
+++ pages/consumer/follow-business.js
@@ -1,68 +1,36 @@
-import { Button } from "@nextui-org/react";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { prisma } from "../../db/prismaDB";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]";
+
+export async function getServerSideProps(context) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  const { id } = context.params;
+
+  try {
+    const consumer = await prisma.consumer.findUnique({
+      where: { email: session.user.email },
+      select: { id: true },
+    });
+
+    const follow = await prisma.follow.create({
+      data: {
+        businessId: id,
+        consumerId: consumer.id,
+      },
+    });
+
+    return {
+      redirect: {
+        destination: `https://fashionpal.app/consumer}`,
+        permanent: false,
+      },
+    };
+  } catch (error) {}
+}
 
 const BusinessPage = () => {
-  const router = useRouter();
-
-  const [businessData, setBusinessData] = useState();
-
-  const fetchBusinessData = async (id) => {
-    const response = await fetch(`/api/business/fetchData/${id}`);
-
-    if (!response.ok) {
-    }
-
-    const data = await response.json();
-    setBusinessData(data);
-  };
-
-  const onFollow = async () => {
-    const response = await fetch("/api/consumer/followBusiness", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ businessId: businessData.id, hash: "" }),
-    });
-
-    if (!response.ok) {
-    }
-    return;
-  };
-
-  const onUnfollow = async () => {
-    const response = await fetch("/api/consumer/unfollowBusiness", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ businessId: businessData.id, hash: "" }),
-    });
-
-    if (!response.ok) {
-    }
-    return;
-  };
-
-  useEffect(() => {
-    const initialFetch = async (id) => {
-      await fetchBusinessData(id);
-    };
-    if (router.query.id) {
-      initialFetch(router.query.id);
-    }
-  }, [router.query]);
-  return (
-    <div>
-      <h6>Business</h6>
-      {businessData?.isFollowing ? (
-        <Button onClick={onUnfollow}>Unfollow</Button>
-      ) : (
-        <Button onClick={onFollow}>Follow</Button>
-      )}
-    </div>
-  );
+  return <div>Following</div>;
 };
 
 export default BusinessPage;
