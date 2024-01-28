199abc4d9a9f8a7a84220cc32a17e1281e3d66b7
immutable-bee <immutablebee@gmail.com>
2024-01-27 21:14:24 -0700

    Add: ximilar collection sync route

---
+++ pages/api/ai/ximilar/syncCollection.js
@@ -0,0 +1,46 @@
+import { prisma } from "../../../../db/prismaDB";
+import { verifySignature } from "@upstash/qstash/dist/nextjs";
+
+// limit of 10 listings per ximilar insert call
+
+const handler = async (req, res) => {
+  const endPoint = "https://api.ximilar.com/similarity/fashion/v2/insert";
+  const headers = {
+    Authorization: `Token ${process.env.XIMILARTOKEN}`,
+    "collection-id": process.env.XIMILAR_COLLECTION_TOKEN,
+    "Content-Type": "application/json;charset=UTF-8",
+  };
+
+  try {
+    const listingsToSync = await prisma.listing.findMany({
+      where: { isSyncedWithXimilar: false },
+      take: 10,
+    });
+
+    const listingMap = listingsToSync.map((listing) => ({
+      _id: listing.id,
+      _url: listing.mainImageUrl,
+    }));
+
+    const payload = {
+      fields_to_return: [_id],
+      records: [listingMap],
+    };
+
+    const ximilarCall = await fetch(endPoint, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(payload),
+    });
+
+    if (!ximilarCall.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    res.status(200).json({ message: "Batch synced with collection" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export default verifySignature(handler);
