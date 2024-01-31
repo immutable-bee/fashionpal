a7185486ae80edd70cb6b9eaf79590fcd7efa895
immutable-bee <immutablebee@gmail.com>
2024-01-30 17:53:02 -0700

    fix: update liquidation response

--- pages/api/business/liquidationRules/update.js
+++ pages/api/business/liquidationRules/update.js
@@ -49,6 +49,7 @@ const handler = async (req, res) => {
         }
       })
     );
+    res.status(200).json();
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
