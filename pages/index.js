6e2cebf58bbb138fbe41c30c6abbd8a4a744ed99
immutable-bee <immutablebee@gmail.com>
2024-02-14 00:06:23 -0700

    Add: init homepage & images

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
+const HomeHeader = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <div className="w-full bg-white flex justify-between p-2">
+      <div className="pt-2">
+        {isMenuOpen ? (
+          <img src="/images/x-cross.svg" />
+        ) : (
+          <img src="/images/hamburger-menu.svg" />
+        )}
+      </div>
+
+      <div>
+        <img width={250} height={250} src="/images/logo.png" />
+      </div>
+
+      <div className="pt-2">
+        <img src="/images/person.svg" />
+      </div>
+    </div>
+  );
+};
+
 const main = () => {
-  return "";
+  return <HomeHeader />;
 };
 
 export default main;
