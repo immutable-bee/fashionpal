56679c68342126de51c4826a4d6c294bbe9493e1
theibrahim <iamibrahimmemon@gmail.com>
2024-02-20 17:11:21 +0500

    website

--- pages/index.js
+++ pages/index.js
@@ -32,12 +32,19 @@ const HomeHeader = () => {
         <WebsiteHeader />
       </div>
 
-      <div className="h-[30rem] sm:h-[calc(100vh-6rem)] bg-[url('../assets/website/cover.png')] bg-cover bg-no-repeat flex items-center justify-center">
-        <div className="bg-white sm:mx-auto max-w-4xl rounded-xl sm:rounded-3xl min-h-[13rem] sm:min-h-[20rem] flex items-center justify-center px-8 sm:px-4 sm:w-full w-[90%] py-4 sm:py-5">
+      <div className="h-[35rem] sm:h-[calc(100vh-6rem)] bg-[url('../assets/website/cover.png')] bg-cover bg-no-repeat flex items-center justify-center">
+        <div className="bg-white sm:mx-auto max-w-4xl rounded-xl sm:rounded-3xl min-h-[20rem] sm:min-h-[20rem] flex items-center justify-center px-8 sm:px-4 sm:w-full w-[90%] py-4 sm:py-5">
           <div>
-            <h3 className="text-gray-500 text-xl sm:text-3xl text-center">
+            <h3 className="text-gray-700 text-xl sm:text-3xl text-center">
               Inventory Management Software Tailored to Second Hand Fashion
             </h3>
+            <p className="text-gray-500 text-center mt-3 text-base sm:text-lg">
+              Your digital fashion ally. Tailored to your unique needs, it
+              transforms customer insights into success strategies. Elevate your
+              operations, boost profitability, and enhance customer
+              satisfaction, all in one sleek platform. Navigate fashion's
+              complexities with ease and watch your business flourish
+            </p>
 
             <div className="flex justify-center items-center mt-6 gap-4">
               <Link href="/auth">
@@ -130,7 +137,7 @@ const HomeHeader = () => {
           </p>
         </div>
         <div className="mt-20">
-          <div className="sm:flex sm:border-none border rounded-3xl overflow-hidden my-12">
+          <div className="sm:flex sm:border-non border rounded-3xl overflow-hidden my-12">
             <div className=" sm:w-1/3">
               <Image
                 src={SmartPricingImage}
@@ -150,7 +157,7 @@ const HomeHeader = () => {
               </div>
             </div>
           </div>
-          <div className="sm:flex sm:border-none border sm:flex-row-reverse rounded-3xl overflow-hidden my-12">
+          <div className="sm:flex sm:border-non border sm:flex-row-reverse rounded-3xl overflow-hidden my-12">
             <div className=" sm:w-1/3">
               <Image
                 src={ProfitableDiscountingImage}
@@ -171,7 +178,7 @@ const HomeHeader = () => {
               </div>
             </div>
           </div>
-          <div className="sm:flex sm:border-none border rounded-3xl overflow-hidden my-12">
+          <div className="sm:flex sm:border-non border rounded-3xl overflow-hidden my-12">
             <div className=" sm:w-1/3">
               <Image
                 src={OneTouchCheckoutImage}
@@ -192,7 +199,7 @@ const HomeHeader = () => {
               </div>
             </div>
           </div>
-          <div className="sm:flex sm:border-none border sm:flex-row-reverse rounded-3xl overflow-hidden my-12">
+          <div className="sm:flex sm:border-non border sm:flex-row-reverse rounded-3xl overflow-hidden my-12">
             <div className=" sm:w-1/3">
               <Image
                 src={RelevantMarketingImage}
@@ -212,7 +219,7 @@ const HomeHeader = () => {
               </div>
             </div>
           </div>
-          <div className="sm:flex sm:border-none border rounded-3xl overflow-hidden my-12">
+          <div className="sm:flex sm:border-non border rounded-3xl overflow-hidden my-12">
             <div className=" sm:w-1/3">
               <Image
                 src={AnalyticsImage}
