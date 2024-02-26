3ef2acad6ed1437c2152321a76428a52e6477e50
immutable-bee <immutablebee@gmail.com>
2024-02-26 06:26:40 -0700

    Update: unfollow logic

--- pages/api/consumer/unfollowBusiness.js
+++ pages/api/consumer/unfollowBusiness.js
@@ -1,6 +1,7 @@
 import { prisma } from "../../../db/prismaDB";
 import { authOptions } from "../auth/[...nextauth]";
 import { getServerSession } from "next-auth";
+import { Client, Environment } from "square";
 
 const handler = async (req, res) => {
   const session = await getServerSession(req, res, authOptions);
@@ -47,7 +48,41 @@ const handler = async (req, res) => {
       });
     }
 
-    res.status(200).json({ message: "Store successfully followed" });
+    const business = await prisma.business.findUnique({
+      where: { id: businessId },
+      select: { squareAccessToken: true },
+    });
+
+    const squareAccessToken = AES.decrypt(
+      business.squareAccessToken,
+      process.env.NEXTAUTH_SECRET
+    ).toString(enc.Utf8);
+    const client = new Client({
+      accessToken: squareAccessToken,
+      environment: Environment.Production,
+    });
+
+    const customer = await client.customersApi.searchCustomers({
+      query: {
+        filter: {
+          emailAddress: {
+            exact: consumer.email,
+          },
+        },
+      },
+    });
+
+    const customerGroup = await client.customerGroupsApi.listCustomerGroups();
+
+    const customerId = customer.result.customers[0].id;
+    const customerGroupId = customerGroup.result.groups[0].id;
+
+    const removeFromGroup = await client.customersApi.removeGroupFromCustomer(
+      customerId,
+      customerGroupId
+    );
+
+    res.status(200).json({ message: "Store successfully Unfollowed" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
