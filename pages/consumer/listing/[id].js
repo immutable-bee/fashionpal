3b9f3a40e326f2fca06ca5b0d2e42ff32aa570b6
theibrahim <iamibrahimmemon@gmail.com>
2024-03-01 16:48:10 +0500

    info message

--- pages/consumer/listing/[id].js
+++ pages/consumer/listing/[id].js
@@ -223,18 +223,41 @@ function Scan() {
             </div>
           )}
 
-          <ButtonComponent
-            rounded
-            className="mt-5"
-            full
-            type="submit"
-            disabled={product.isFollowing}
-            onClick={followBusiness}
-          >
-            {product.isFollowing
-              ? `Following ${product.Business?.businessName || "store"}`
-              : `Follow ${product.Business?.businessName || "store"}`}
-          </ButtonComponent>
+          {product.isFollowing ? (
+            <div className="bg-green-200 rounded-lg mt-5 py-3 px-2 flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6 flex-shrink-0 text-gray-600"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+                />
+              </svg>
+
+              <div>
+                <h6 className="text-gray-700">
+                  You are following{" "}
+                  {product.Business?.businessName || "this store"}.
+                </h6>
+              </div>
+            </div>
+          ) : (
+            <ButtonComponent
+              rounded
+              className="mt-5"
+              full
+              type="submit"
+              onClick={followBusiness}
+            >
+              Follow {product.Business?.businessName || "this store"}
+            </ButtonComponent>
+          )}
         </>
       )}
     </div>
